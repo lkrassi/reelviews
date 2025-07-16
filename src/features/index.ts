87e89708cf2836a8c59ca13604dc72c6c819311f
Leonid <firefoxwolf0824@gmail.com>
2025-07-16 17:10:39 +0300

    index (файлы с реэкспортами)

---
+++ src/features/index.ts
@@ -0,0 +1,2 @@
+export * from './ui/auth/RegisterForm';
+export * from './ui/auth/LogInForm';
---
+++ src/widgets/index.ts
@@ -0,0 +1,4 @@
+export * from './theme/ThemeSwitcher';
+export * from './notifications/NotificationProvider';
+export * from './hooks/useModal';
+export * from './hooks/useNotifications';
